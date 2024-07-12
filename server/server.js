dc7bab74575f81d1c976ed13d7a12040f206f276
Alice Smith <alice@webage.com>
2024-07-12 10:30:04 -0400

    added barebones server functionality

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
 // node server
+import express from 'express';
+
+const app = express();
+// Middleware to parse JSON bodies
+app.use(express.json());
+const PORT = 3000;
+
+// start endpoint
+app.get('/', async (req, res) => {
+    try {
+        res.json("HELLO WORLD")
+    } catch (err) {
+        console.error("Error:", err);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
