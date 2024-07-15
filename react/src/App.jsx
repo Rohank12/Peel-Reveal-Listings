030d0305d143d568c84fce6cfc03a5659beca282
Rohank12 <rohan_kelley@uri.edu>
2024-07-15 09:27:56 -0400

    added prediction route and form

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Home from './components/Home'
+import PredictForm from './components/PredictForm'
 import Search from './components/Search'
 import EmployeeList from './components/EmployeeList'
 import Login from './components/Login'
@@ -19,6 +20,7 @@ const App = () => {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/employees" element={<EmployeeList />} />
+          <Route exact path="/predict" element={<PredictForm />} />
         </Routes>
         </AuthProvider>
       </div>
