6364f32a18880f9033d0e3be2e24b3be68a5642f
Rohank12 <rohan_kelley@uri.edu>
2024-07-15 08:54:37 -0400

    added predict script to receive cl args

---
+++ python-scripts/predict.py
@@ -0,0 +1,34 @@
+import pickle
+import sys
+import pandas as pd
+
+# load the model
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+# load the column names
+with open('columns.pkl', 'rb') as file:
+    train_columns = pickle.load(file)
+
+# get the parameters from command line arguments
+parameters = sys.argv[1:]
+print(parameters)
+
+# create a dataframe from the parameters
+df = pd.DataFrame([parameters], columns=['jobRole', 'location'])
+#print(df)
+
+# one-hot encode the dataframe
+df_encoded = pd.get_dummies(df)
+#print(df_encoded)
+
+# add the missing columns from the original training set
+df_encoded = df_encoded.reindex(columns=train_columns, fill_value=0)
+
+# make a prediction
+prediction = model.predict(df_encoded)
+
+# round prediction to whole number
+prediction = int(round(prediction[0], 2))
+
+print(prediction)
